2654abfe6c3e1c8ff7de5e4b53793de6fcb1a59f
Yuzrijal <muhammadyusrizal28@gmail.com>
2022-01-18 11:27:22 +0700

    Homepage

---
+++ Source.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Menampilkan home page yang berisi list menu yang terdapat pada program cinekom 
+	cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMMMMMMMMMMWWXd:;dKWMMMMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMWX0XWMMMMN0xoc,,,;cd0NWMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMN0xc;cOWWKkl;,,,,,,,,,;lxKWMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMWKkl;,:lxxxo:,,,,,,,,;:lol:,:okXWMMMMMMMMM" << endl;
+	cout << "MMMMMWNOo:,;cdxdl;,,,,,,,,,:ldxkkkxoc;;:dOXWMMMMMM" << endl;
+	cout << "MMMN0xc;;:oxxo:,',,,,,,;cldOXKOxxdoloooc;;cxNMMMMM" << endl;
+	cout << "WKkl;,,:dkkkxc,'',,,;:oOXNNWMMWXkc,',;codlcxNMMMMM" << endl;
+	cout << "Xl,,,,,:xOxxkOOl,,;lkKNWMMMMMMW0c,',,,,,:okXMMMMMM" << endl;
+	cout << "Nx,,,,,,lkOKNWMXkx0XWWWWWWWMMMW0o:,,,,,,,,;lkXWMMM" << endl;
+	cout << "MXl,,,,,;oKWMMMMMWWNXK00000KXNWWNKko:,,,,,,,,:dONW" << endl;
+	cout << "MWO:,,,,,:xXMMMMWNKOkkkxxxxkkOKNWWWN0xl;,,,,,,,,cx" << endl;
+	cout << "MMNx,,,,,,l0NMMWX0kxxxxxxxxxxxk0XWMMWWXOdc;,,,,,,;" << endl;
+	cout << "KO0Oc,,,,,;dKWMWKOkxxxdddddxxxxOKNMMMMMWKkdl:,,,;x" << endl;
+	cout << "c;ckd;,,,,,:kNWNKOkxxxdooodxxxxO0NMMMMMNOdkOkc,,lK" << endl;
+	cout << "d;;okl,',,',c0WWX0kxxxxxxxxxxxkOXWWKO0KOxdxOxc,:OW" << endl;
+	cout << "Kl,:dx:,,;ldOXWMWX0Okkxxxxxxkk0XNW0l,,;;:cxkl,;xNM" << endl;
+	cout << "WO:,cxxood0WMMMMMWNXK0OOOOO0KXNWWXd;,,''':xd:,lKMM" << endl;
+	cout << "MNx;,lkOkxkXWWWWWWWWNNXXNNNWWMMMNk:,,,,',oxc,:OWMM" << endl;
+	cout << "MMKl,;lddoodddddddddddod0NWMMMMW0l,,,,,,lko;;dNMMM" << endl;
+	cout << "MMWOc,,,,,,,,,,,,,,,,,',dXNMMMWKd;,,,,,cO0xldKMMMM" << endl;
+	cout << "MMMNx;,,,,,,,,,,,,,,,,',lkO0000x:,,,,,:OWMWWWMMMMM" << endl;
+	cout << "MMMMXo;;;;;;;;;;;;;;;;;;cddxxkkl,,,,,,dNMMMMMMMMMM" << endl;
+	cout << "MMMMWNKKKKKKKKKKkdxxdxxxxkkkkko;,,,,,lKMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMNx:::::::::::::;,,,,,:OWMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMM:;;;;;;;;;;;;;,,,,,MMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" << endl;
+	cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" << endl << endl;
+	
+	cout << endl;
+	cout << endl;
+
+	cout << "**** **** *        *     **   **     *     *****     ***       *     *****     *     *   *  ****" << endl;
+	cout << "*    *    *       * *    * * * *    * *      *       *  *     * *      *      * *    **  * *" << endl;
+	cout << "**** **** *      *   *   *  *  *   *   *     *       *   *   *   *     *     *   *   * * * *****" << endl;
+	cout << "   * *    *     *******  *     *  *******    *       *  *   *******    *    *******  *  ** *   *" << endl;
+	cout << "**** **** **** *       * *     * *       *   *       ***   *       *   *   *       * *   *  ***" << endl;
+
+	cout << endl;
+
+	cout << "                                              ***   *" << endl;
+	cout << "                                              *  *  *" << endl;
+	cout << "                                              *   * *" << endl;
+	cout << "                                              *  *  *" << endl;
+	cout << "                                              ***   *" << endl;
+
+	cout << endl;
+	cout << endl;
+
+	  
+	cout << "               *     ***   *   * *   *     *     *    **   **     *     ****  *****" << endl;
+	cout << "              * *    *  *  *   * *   *    * *    *    * * * *    * *    *   *   *" << endl;
+	cout << "             *   *   *   * *   * *****   *   *   *    *  *  *   *   *   ****    *" << endl;
+	cout << "            *******  *  *  *   * *   *  *******  *    *     *  *******  *  *    *" << endl;
+	cout << "           *       * ***    ***  *   * *       * *    *     * *       * *   *   *" << endl;
+
+
+	cout << endl;
+	cout << endl;
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+
+
+	
+
+
+	cout << " " << endl;
+	cout << "|=========================================================================================================================|" << endl;
+}
+
